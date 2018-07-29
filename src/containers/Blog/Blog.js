1023a4da8649895edf25611218b1902281f32d05
Jet Balagtas <jetbalagtas@gmail.com>
2018-07-29 17:11:16 -0400

    transform data after receipt, then setState

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,14 +14,24 @@ class Blog extends Component {
   componentDidMount () {
     axios.get('http://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        this.setState({posts: response.data});
+        // there seems to be too many posts from our demo REST server
+        // in a real application, you can send some query parameters to your backend to restrict the amount of data retrieved, basically pagination
+        // alternatively, we can also transform the data when we have it, like so:
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        });
+        this.setState({posts: updatedPosts});
         // console.log(response);
       });
   }
 
   render () {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} />;
+      return <Post key={post.id} title={post.title} author={post.author} />;
     });
 
     return (
