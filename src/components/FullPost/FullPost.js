a97b3f0c9ee5e37f63c9dc8e2480bdd3eae488c6
Jet Balagtas <jetbalagtas@gmail.com>
2018-07-29 18:08:47 -0400

    fetch data on componentDidUpdate without creating infinite loops of get requests

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+  state = {
+    loadedPost: null
+  }
+
+  componentDidUpdate () {
+    if (this.props.id) {
+      // avoid an infinite loop of get requests because we are setting state within componentDidUpdate, which will make componentDidUpdate run again and again
+      // so, check if we don't have a loadedPost or if there is one, that it's not equal to the one that's already loaded
+      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+        axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+          .then(response => {
+            // console.log(response);
+            this.setState({loadedPost: response.data})
+          });
+      }
+    }
+  }
+
   render () {
     let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
     if (this.props.id) {
+      post = <p style={{textAlign: 'center'}}>Loading...</p>;
+    }
+    if (this.state.loadedPost) {
       post = (
         <div className="FullPost">
-          <h1>Title</h1>
-          <p>Content</p>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
           </div>
