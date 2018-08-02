f678ef97f130975f82306a33b1afd24ac5b88512
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-01 20:05:13 -0400

    add interceptors to execute code globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+  // you can also Edit the request config such as add headers, etc.
+  return request;
+  // for the when the request sending fails
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+// so we still forward the error to our component, where we are catching the error
+axios.interceptors.response.use(response => {
+  console.log(response);
+  // Edit request config
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
