b1c457267fd6b5170981cc6353e94841f3a02a36
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-06 15:00:46 -0400

    use links to switch between pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -19,8 +19,12 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to={{
+                pathname: '/new-post',
+                hash: '#submit', // example if setting up the path
+                search: '?quick-submit=true' // example if setting up the path
+              }}>New Post</Link></li>
             </ul>
           </nav>
         </header>
