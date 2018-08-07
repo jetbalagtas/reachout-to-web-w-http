177548fd27878f57d9e766a493af24d2f2fbb4ea
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-07 13:31:05 -0400

    lazy loading / code splitting for bigger apps with asynchronous component loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,17 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
 // import FullPost from './FullPost/FullPost';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
   state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-    auth: false
+    auth: true
   }
 
   render () {
@@ -40,7 +42,7 @@ class Blog extends Component {
         {/* <Route path='/' exact render={() => <h1>Home</h1>} />
         <Route path='/' render={() => <h1>Home 2</h1>} /> */}
         <Switch>
-          {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null}
+          {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null}
           <Route path='/posts' component={Posts} />
           <Route render={() => <h1>Not found</h1>} /> {/* will catch any route prior to this that is unknown and won't work with a redirect from root route (below) */}
           {/* <Redirect from='/' to='/posts' /> */} {/* this also catches all unknown routes */}
