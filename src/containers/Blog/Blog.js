18a831bca78e3719775f861661b7b19534f781ff
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-06 21:04:20 -0400

    understanding nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+// import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
   state = {
@@ -21,13 +21,13 @@ class Blog extends Component {
           <nav>
             <ul>
               <li><NavLink // the key takeaway is that the NavLink automatically attaches the 'active' class
-                to='/'
+                to='/posts/'
                 exact
                 activeClassName='my-active' // but you can also use a custom class
                 activeStyle={{ // or you can also use inline styling
                   color: '#fa923f',
                   textDecoration: 'underline'
-                }}>Home</NavLink></li>
+                }}>Posts</NavLink></li>
               <li><NavLink to={{
                 pathname: '/new-post', // absolute path. relative path: this.props.match.url + '/new-post'
                 hash: '#submit', // example if setting up the path
@@ -38,10 +38,10 @@ class Blog extends Component {
         </header>
         {/* <Route path='/' exact render={() => <h1>Home</h1>} />
         <Route path='/' render={() => <h1>Home 2</h1>} /> */}
-        <Route path='/' exact component={Posts} />
         <Switch>
-          <Route path='/new-post' exact component={NewPost} />
-          <Route path='/:id' exact component={FullPost} />
+          <Route path='/new-post' component={NewPost} />
+          <Route path='/posts' component={Posts} />
+          {/* <Route path='/:id' exact component={FullPost} /> */}
         </Switch>
         {/* instead of altering the path for example to path='/posts/:id', you can simply use Switch which loads one route at a time */}
       </div>
